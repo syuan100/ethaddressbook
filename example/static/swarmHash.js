010b4ca24f8ae85d5630486516c880e662f60dd0
Steven Yuan <syuan100@gmail.com>
2018-10-29 15:33:36 -0700

    Logging to show what the client is waiting for

--- example/static/swarmHash.js
+++ example/static/swarmHash.js
@@ -67,8 +67,14 @@ function init() {
   document.querySelector("#get-address-book").addEventListener("click", async function(){   
     let currentHash = document.getElementById("hash-for-address").innerHTML
     let password = document.querySelector("#decrypt-password").value
+    console.log("awaiting response from Swarm...")
     let addressBook = await ethAddressBook.getBook(currentHash, password)
+    console.log("Swarm responded.")
     document.querySelector("#stored-text-box").value = addressBook
   })
+
+  // TODO
+  // - After storing addressbook, show client waiting for transaction to confirm 
+  // - Show client waiting for response from storage (Swarm / IPFS)
 
 }
