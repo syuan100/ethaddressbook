3848545e7236160f39a5ccd801226091ad6d33af
Steven Yuan <syuan100@gmail.com>
2018-11-17 14:52:27 -0800

    Add pages and static files from web example

---
+++ example-nodejs/pages/index.js
@@ -0,0 +1,10 @@
+import Main from '../components/Main.js'
+import Header from '../components/Header.js'
+import ContractTest from '../components/ContractTest.js'
+
+export default () => (
+  <Main>
+    <Header></Header>
+    <ContractTest></ContractTest>
+  </Main>
+)
---
+++ example-nodejs/static/swarmHash.js
@@ -0,0 +1,80 @@
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+
+    // Use the browser's ethereum provider
+    var provider = web3.currentProvider
+    console.log("Connected to web3 provider.")
+    web3.version.getNetwork((err, netId) => {
+      switch (netId) {
+        case "1":
+          console.log('mainnet')
+          break
+        case "2":
+          console.log('On the deprecated Morden test network.')
+          break
+        case "3":
+          console.log('On the ropsten test network.')
+          ethAddressBook.setNetwork(netId)
+          break
+        case "4":
+          console.log('On the Rinkeby test network.')
+          ethAddressBook.setNetwork(netId)
+          break
+        case "42":
+          console.log('On the Kovan test network.')
+          break
+        default:
+          console.log('On an unknown network.')
+      }
+      init()
+    })
+
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+  }
+
+})
+
+function init() {
+
+  async function updateInterface(account) {
+    document.getElementById("current-address").innerHTML = account
+    document.getElementById("hash-for-address").innerHTML = await ethAddressBook.getHash(account)
+  }
+
+  var account = web3.eth.accounts[0]
+
+  var accountInterval = setInterval(function() {
+    if (web3.eth.accounts[0] !== account) {
+      account = web3.eth.accounts[0]
+      updateInterface(account)
+    }
+  }, 100)
+
+  if (account) {
+    updateInterface(account)
+  }
+
+  document.querySelector("#encrypt").addEventListener("click", async function(e){
+    e.preventDefault()
+    var payload = document.querySelector("#data-to-encrypt").value
+    var password = document.querySelector("#encrypt-password").value
+    ethAddressBook.storeBook(account, payload, password)
+  })
+
+  document.querySelector("#get-address-book").addEventListener("click", async function(){   
+    let currentHash = document.getElementById("hash-for-address").innerHTML
+    let password = document.querySelector("#decrypt-password").value
+    console.log("awaiting response from Swarm...")
+    let addressBook = await ethAddressBook.getBook(currentHash, password)
+    console.log("Swarm responded.")
+    document.querySelector("#stored-text-box").value = addressBook
+  })
+
+  // TODO
+  // - After storing addressbook, show client waiting for transaction to confirm 
+  // - Show client waiting for response from storage (Swarm / IPFS)
+
+}
